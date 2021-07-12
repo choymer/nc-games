f85b02e2599b9f3d2db1c8ebae0e829d90159103
Mercedes <mercedeschoy@gmail.com>
2021-07-12 12:42:06 +0100

    Added components and initial layout in boxes

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className="nav">
+            <h1>navbar</h1>
+        </nav>
+    );
+};
+
+export default Navbar;
---
+++ src/components/Reviews.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Reviews = () => {
+    return (
+        <div className="content">
+            <ul>
+                <li>review 1</li>
+                <li>review 2</li>
+                <li>review 3</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Reviews;
