76d7ddc625cbde68cb08778d63df245c4cef7d5d
Mercedes <mercedeschoy@gmail.com>
2021-07-13 13:02:47 +0100

    Refactored to be optimistic rendering when a vote is added

--- src/components/AddVotes.jsx
+++ src/components/AddVotes.jsx
@@ -3,23 +3,23 @@ import { patchVotes } from '../utils/api';
 
 const AddVotes = ({votes,reviewId}) => {
 
-    // console.log(votes, '<<<votes')
-    // console.log(votes, '<<<reviewid')
   const [incVotes, setIncVotes] = useState(0);
+  const [hasError, setHasError] = useState(false);
 
   const incrementVotes= () => {
-      patchVotes(reviewId, 1).then(()=>{
-      setIncVotes((currentVotes) => {
+    setHasError(false);  
+    setIncVotes((currentVotes) => {
         return currentVotes + 1;
       })
+      patchVotes(reviewId, 1).catch((err)=>{
+        setHasError(true);
+        setIncVotes(0);
       })
   }
-
-  
- 
     return (
         <div>
         <p>Votes: {votes + incVotes}</p>
+        {hasError && <p>Oops! Something wrong happened, check your connection and try again!</p>}
         <button onClick={incrementVotes}>Add votes</button>
         </div>
     );
